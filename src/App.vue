714f5ce4be870ff997c0c2420908cc776d0db6de
Edouard-Hashimoto <edouard.hashimoto@edu.univ-fcomte.fr>
2024-04-12 14:36:11 +0200

    Ajout transition

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,20 @@ const menuIsOpen = ref(false)
     @pointerdown="menuIsOpen = !menuIsOpen">
     menu
     </button>
-  <nav id="mainNav" v-show="menuIsOpen">
+  <Transition
+    class="transition-transform duration-1000"
+    enter-from-class="-translate-x-full"
+    enter-to-class="translate-x-0"
+    leave-active-class="-translate-x-full"
+  >   
+   <nav id="mainNav" v-show="menuIsOpen">
     <ul>
       <li><RouterLink to="/">item 1</RouterLink></li>
       <li><RouterLink to="/">item 2</RouterLink></li>
       <li><RouterLink to="/">item 3</RouterLink></li>
     </ul>
   </nav>
+</Transition>
+
 </header>
 </template>
